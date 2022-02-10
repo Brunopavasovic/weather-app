43b2ab33eff0d6b911ddb63b648ef17346287f81
Bruno Pavasović <brunopavasovic@gmail.com>
2022-02-10 13:54:44 +0100

    created ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import City from "./components/City";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="search">
+          <label>Search your city :</label>
+          <input
+            type="text"
+            className="input-bar"
+            placeholder="search city..."
+          ></input>
+          <button className="btn" type="submit">
+            Search
+          </button>
+        </div>
+        <City />
+      </div>
     </div>
   );
 }
