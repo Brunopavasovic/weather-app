e6d93c631bfb48ab67c735c65dcc22cf6a112e8a
Bruno Pavasović <brunopavasovic@gmail.com>
2022-02-11 13:14:27 +0100

    added api and theme changer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,106 @@
+import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const api = {
+    key: "07107f87d11ad7755cf382979190c8c3",
+    base: "http://api.openweathermap.org/data/2.5/",
+  };
+
+  const fetchData = () => {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+        console.log(result);
+      });
+  };
+
+  const addDate = (d) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const day = days[d.getDay()];
+    const month = months[d.getMonth()];
+    const date = d.getDate();
+    const year = d.getFullYear();
+
+    return `${day} , ${month} ${date} , ${year}.`;
+  };
+
   return (
-    <div className="App">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "warm"
+            : "App"
+          : "App"
+      }
+    >
       <div className="container">
         <div className="search">
           <label>Search your city :</label>
           <input
+            id="city-list"
             type="text"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
             className="input-bar"
             placeholder="search city..."
           ></input>
-          <Button />
+          <datalist id="city-list">{weather.name}</datalist>
+          <button onClick={fetchData} className="btn" type="submit">
+            Search
+          </button>
         </div>
-        <div className="show-city-name">
-          <h1>
-            Zagreb,<span>CRO</span>
-          </h1>
-        </div>
-        <div className="show-date">
-          <h3>date</h3>
-        </div>
+        {weather.main ? (
+          <div className="data-container">
+            <div className="show-city-name">
+              <h1>
+                {weather.name},<span>{weather.sys.country}</span>
+              </h1>
+            </div>
+            <div className="show-date">
+              <h3>{addDate(new Date())}</h3>
+            </div>
+            <div className="celsius">
+              <h1 className="show-celsius">
+                {Math.round(weather.main.temp)}°c
+              </h1>
+            </div>
+            <div className="sky">
+              <h3 className="show-sky">{weather.weather[0].main}</h3>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
